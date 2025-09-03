f9a6c3214aa6cf752a34fa23452ae060ce449dce
kimboyun00 <bykim1827@gmail.com>
2025-09-03 19:26:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def auth_css():
         div[data-testid="stButton"] > button {
             width: 100%;
             padding: 12px 0;
-            background: linear-gradient(45deg, #00c6ff, #0072ff);
+            background: linear-gradient(45deg, #4CAF50, #8BC34A);
                 border: none;
                 border-radius: 10px;
                 color: white;
